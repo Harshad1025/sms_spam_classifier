27c6252d6592eab37188eb03d784b53226bff423
Harshad Thombre <harshadthombre25@gmail.com>
2024-06-01 17:31:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import pickle
 import string
 from nltk.corpus import stopwords
 import nltk
-from scipy.sparse import csr_matrix
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
-nltk.download('stopwords')
-ps = PorterStemmer()
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# Download NLTK resources
 nltk.download('punkt')
 nltk.download('stopwords')
+
+# Initialize Porter Stemmer
 ps = PorterStemmer()
 
 # Load spam image
@@ -20,7 +21,13 @@ spam_image = open("spam_image.png", "rb").read()
 spam_emoji = "🚨"
 not_spam_emoji = "✅"
 
-# @st.cache_data
+# Load the trained model and vectorizer
+with open('vectorizer.pkl', 'rb') as vectorizer_file:
+    tfidf = pickle.load(vectorizer_file)
+
+with open('model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -45,37 +52,19 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
-
-initial_input_sms_value = ""  # Initial value for input text area
-
+# Streamlit UI
 st.title("Email/SMS Spam Classifier")
 
-# Unique key for the input text area
-input_sms_key = "input_sms"
+# Input text area
+input_sms_value = st.text_area("Enter the message")
 
-# Use a separate variable to store the value of the text area
-input_sms_value = st.text_area("Enter the message", key=input_sms_key, value=initial_input_sms_value)
-
-# Create an empty placeholder for displaying the output
-output_placeholder = st.empty()
-
+# Display predictions
 if st.button('Predict'):
-
-    # 1. preprocess
     transformed_sms = transform_text(input_sms_value)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])  # Directly transform using tfidf
-    # 3. predict
+    vector_input = tfidf.transform([transformed_sms])
     result = model.predict(vector_input)[0]
-    # 4. Display
     if result == 1:
-        output_placeholder.error(f"{spam_emoji} Spam")
-        st.image(spam_image, width=150)  # Display a smaller image
+        st.error(f"{spam_emoji} Spam")
+        st.image(spam_image, width=150)
     else:
-        output_placeholder.success(f"{not_spam_emoji} Not Spam")
-
-# Add a home button
-if st.button("Home"):
-    input_sms_value = ''
+        st.success(f"{not_spam_emoji} Not Spam")
